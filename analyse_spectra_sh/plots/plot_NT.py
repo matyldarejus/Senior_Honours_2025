bb00c7cb6c4e0a014c043849cd35501817a574ee
Matylda R. <matyldarejus@icloud.com>
2025-11-11 15:19:34 +0000

    Fixed plots for single ion

--- analyse_spectra_sh/plots/plot_NT.py
+++ analyse_spectra_sh/plots/plot_NT.py
@@ -85,8 +85,10 @@ if __name__ == '__main__':
     cmap = truncate_colormap(cmap, 0.1, 0.9)
     norm = colors.BoundaryNorm(np.arange(0.125, 1.625, 0.25), cmap.N)
 
+    """
     fig, ax = plt.subplots(len(lines), 3, figsize=(14, 13), sharey='row', sharex='col')
 
+    
     rho_lines = []
     rho_lines.append(Line2D([0,1],[0,1], color='plum', ls='--', lw=2))
     rho_lines.append(Line2D([0,1],[0,1], color='plum', ls='-', lw=2))
@@ -180,3 +182,98 @@ if __name__ == '__main__':
     fig.subplots_adjust(wspace=0., hspace=0.)
     plt.savefig(f'{plot_dir}{model}_{wind}_{snap}_NT_r200.png', format='png')
     plt.close()
+
+    """
+    # Single plot for OVI only
+
+    fig, ax = plt.subplots(1, 3, figsize=(14, 4.5), sharey='row', sharex='col')
+    rho_lines = []
+    rho_lines.append(Line2D([0,1],[0,1], color='plum', ls='--', lw=2))
+    rho_lines.append(Line2D([0,1],[0,1], color='plum', ls='-', lw=2))
+    leg = ax[0].legend(rho_lines, rho_labels, loc=4, fontsize=12)
+    ax[0].add_artist(leg)
+
+    line = "OVI1031"
+    results_file = f'/disk04/mrejus/sh/normal/results/{model}_{wind}_{snap}_hm12_fit_lines_{line}.h5'
+    N = []
+    T = []
+    chisq = []
+    ids = []
+    all_r = []
+    
+    for i in range(len(fr200)):
+
+        with h5py.File(results_file, 'r') as hf:
+            N.extend(hf[f'log_N_{fr200[i]}r200'][:])
+            T.extend(hf[f'log_T_{fr200[i]}r200'][:])
+            chisq.extend(hf[f'chisq_{fr200[i]}r200'][:])
+            ids.extend(hf[f'ids_{fr200[i]}r200'][:])
+            all_r.extend([fr200[i]] * len(hf[f'ids_{fr200[i]}r200'][:]))
+
+    N = np.array(N)
+    T = np.array(T)
+    chisq = np.array(chisq)
+    ids = np.array(ids)
+    all_r = np.array(all_r)
+
+    mask = (N > N_min[lines.index(line)]) * (chisq < chisq_lim[lines.index(line)])
+    T = T[mask]
+    N = N[mask]
+    all_r = all_r[mask]
+    ids = ids[mask]
+    idx = np.array([np.where(gal_ids == l)[0] for l in ids]).flatten()
+    ssfr = all_ssfr[idx]
+    
+    sf_mask, gv_mask, q_mask = ssfr_type_check(quench, ssfr)
+    inner_mask = all_r < 1.0
+    silly_mask = N < 18.
+    
+    plot_order = np.arange(len(N[sf_mask]))
+    np.random.shuffle(plot_order)
+    im = ax[0].scatter(N[sf_mask][plot_order], T[sf_mask][plot_order], c=all_r[sf_mask][plot_order], cmap=cmap, norm=norm, s=1.5)
+    bin_cent,ymean,ysiglo,ysighi,ndata = pm.runningmedian(N[sf_mask*inner_mask*silly_mask], T[sf_mask*inner_mask*silly_mask], stat='median')
+    ax[0].plot(bin_cent[ndata>nmin], ymean[ndata>nmin], c='plum', lw=2, ls='--')
+    bin_cent,ymean,ysiglo,ysighi,ndata = pm.runningmedian(N[sf_mask*~inner_mask*silly_mask], T[sf_mask*~inner_mask*silly_mask], stat='median')
+    ax[0].plot(bin_cent[ndata>nmin], ymean[ndata>nmin], c='plum', lw=2, ls='-')
+
+    plot_order = np.arange(len(N[gv_mask]))
+    np.random.shuffle(plot_order)
+    im = ax[1].scatter(N[gv_mask][plot_order], T[gv_mask][plot_order], c=all_r[gv_mask][plot_order], cmap=cmap, norm=norm, s=1.5)
+    bin_cent,ymean,ysiglo,ysighi,ndata = pm.runningmedian(N[gv_mask*inner_mask*silly_mask], T[gv_mask*inner_mask*silly_mask], stat='median')
+    ax[1].plot(bin_cent[ndata>nmin], ymean[ndata>nmin], c='plum', lw=2, ls='--')
+    bin_cent,ymean,ysiglo,ysighi,ndata = pm.runningmedian(N[gv_mask*~inner_mask*silly_mask], T[gv_mask*~inner_mask*silly_mask], stat='median')
+    ax[1].plot(bin_cent[ndata>nmin], ymean[ndata>nmin], c='plum', lw=2, ls='-')
+    plot_order = np.arange(len(N[q_mask]))
+
+    np.random.shuffle(plot_order)
+    im = ax[2].scatter(N[q_mask][plot_order], T[q_mask][plot_order], c=all_r[q_mask][plot_order], cmap=cmap, norm=norm, s=1.5)
+    bin_cent,ymean,ysiglo,ysighi,ndata = pm.runningmedian(N[q_mask*inner_mask*silly_mask], T[q_mask*inner_mask*silly_mask], stat='median')
+    ax[2].plot(bin_cent[ndata>nmin], ymean[ndata>nmin], c='plum', lw=2, ls='--')
+    bin_cent,ymean,ysiglo,ysighi,ndata = pm.runningmedian(N[q_mask*~inner_mask*silly_mask], T[q_mask*~inner_mask*silly_mask], stat='median')
+    ax[2].plot(bin_cent[ndata>nmin], ymean[ndata>nmin], c='plum', lw=2, ls='-')
+    ax[0].set_title('Star forming')
+    ax[1].set_title('Green valley')
+    ax[2].set_title('Quenched')
+    ax[0].set_ylabel(r'${\rm log } (T / {\rm K})$')
+    
+    for i in range(3):
+        ax[i].set_xlabel(r'${\rm log }(N / {\rm cm}^{-2})$')
+        ax[i].set_xlim(np.min(N_min), 18)
+        ax[i].set_ylim(3, 6)
+        ax[i].axhline(Tth, ls=':', c='k', lw=1)
+    
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.82, 0.24, 0.02, 0.51])
+    fig.colorbar(im, cax=cbar_ax, ticks=fr200, label=r'$\rho / r_{200}$')
+    fig.subplots_adjust(wspace=0., hspace=0.)
+    plt.savefig(f'{plot_dir}{model}_{wind}_{snap}_OVI_NT_r200.png', format='png')
+    plt.close()
+
+
+                                                 
+                                                            
+                                                 
+
+
+    
+
