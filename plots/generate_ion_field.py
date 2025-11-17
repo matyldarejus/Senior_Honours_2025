629effb0b1fe3a33c6743c02e35881681265fc76
Matylda R. <matyldarejus@icloud.com>
2025-11-17 13:13:56 +0000

    Fix

--- plots/generate_ion_field.py
+++ plots/generate_ion_field.py
@@ -5,4 +5,4 @@ density = [1e-28, 1e-29]
 temperature = [1e5, 1e5]
 redshift = [0, 0]
 
-trident.calculate_ion_fraction('O VI', density, temperature, redshift)
+trident.ion_balance.calculate_ion_fraction('O VI', density, temperature, redshift)
