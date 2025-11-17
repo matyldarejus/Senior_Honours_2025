750516cca000a157391c8cde97964d11a85d188e
Matylda R. <matyldarejus@icloud.com>
2025-11-17 13:53:05 +0000

    Added a projection plot for OVI

--- plots/generate_ion_field.py
+++ plots/generate_ion_field.py
@@ -1,8 +1,25 @@
 import yt
 import trident
+import sys 
 
-density = [1e-28, 1e-29]
-temperature = [1e5, 1e5]
-redshift = [0, 0]
+# Set the arguments
+model = sys.argv[1]
+wind = sys.argv[2]
+snap = sys.argv[3]
 
-trident.ion_balance.calculate_ion_fraction('O VI', density, temperature, redshift)
+# Load the dataset
+fn = "/disk04/rad/sim/{model}/{wind}/"
+ds = yt.load(fn)
+
+# Add an ion field for OVI
+trident.add_ion_fields(ds, ions=['O VI'])
+
+# Initialise the projection plot
+proj = yt.ProjectionPlot(ds, "z", "O_p5_number_density")
+proj.set_cmap("O_p5_number_density", "viridis")
+
+# Save the projection plot
+plot_dir = '/home/matylda/data/plots/'
+output_filename = f'{plot_dir}ion_field_{model}_{wind}_{snap}'
+proj.save(f'{output_filename}.png', format='png', dpi=400)
+proj.save(f'{output_filename}.pdf', format='pdf')
